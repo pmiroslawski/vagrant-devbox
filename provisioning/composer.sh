7a5ec1aaec4d1b7ae2ad7886018547eff243dca1
Paweł Mirosławski <pmiroslawski@gmail.com>
2018-10-16 11:24:17 +0200

    composer.sh updated

--- provisioning/composer.sh
+++ provisioning/composer.sh
@@ -6,12 +6,12 @@ echo "---------------------------------------"
 
 if [ ! -f /usr/bin/composer  ]; 
 then
-  cd /home/dgvip/
+  cd ~/
   wget https://getcomposer.org/installer
   php installer
   sudo mv composer.phar /usr/bin/composer
   sudo chmod 777 /usr/bin/composer
-  rm /home/dgvip/installer
+  rm ~/installer
 else 
   echo "composer installed!"
 fi
