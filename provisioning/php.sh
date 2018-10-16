bc9e476ba54d828f40f48484166b1b5f0e967f50
Paweł Mirosławski <pmiroslawski@gmail.com>
2018-10-16 12:56:18 +0200

    php bcmath extension added

--- provisioning/php.sh
+++ provisioning/php.sh
@@ -4,12 +4,11 @@ echo "---------------------------------------"
 echo "> PHP $PHP"
 echo "---------------------------------------"
 
-packages="libapache2-mod-php$PHP php$PHP php$PHP-cli php$PHP-common php$PHP-curl php$PHP-dev php$PHP-fpm php$PHP-gd php$PHP-intl php$PHP-json php$PHP-mbstring php$PHP-mysql php$PHP-opcache php$PHP-phpdbg php$PHP-readline php$PHP-redis php$PHP-ssh2 php$PHP-soap php$PHP-sqlite3 php$PHP-tidy php$PHP-xml php$PHP-xsl php$PHP-zip"
+packages="libapache2-mod-php$PHP php$PHP php$PHP-cli php$PHP-bcmath php$PHP-common php$PHP-curl php$PHP-dev php$PHP-fpm php$PHP-gd php$PHP-intl php$PHP-json php$PHP-mbstring php$PHP-mysql php$PHP-opcache php$PHP-phpdbg php$PHP-readline php$PHP-redis php$PHP-ssh2 php$PHP-soap php$PHP-sqlite3 php$PHP-tidy php$PHP-xml php$PHP-xsl php$PHP-zip"
 
 if [ ! -f /usr/bin/php ]; 
 then
   apt-get install -y $packages
-  pecl channel-update pecl.php.net
   
   echo " ... HAS BEEN INSTALLED SUCCESSFULLY!"
 else
