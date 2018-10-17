082ac4de2d465448fd13100318dbdb49573b2362
Paweł Mirosławski <pmiroslawski@gmail.com>
2018-10-17 13:23:58 +0200

    git.sh - install/updated separately git & git-flow

--- provisioning/git.sh
+++ provisioning/git.sh
@@ -6,8 +6,20 @@ echo "---------------------------------------"
 
 if [ ! -f /usr/bin/git  ]; 
 then
-  apt-get install -y git git-flow
+  apt-get install
 else
   apt-get install -y --only-upgrade git 
+  echo "GIT already installed"
 fi
 
+echo "---------------------------------------"
+echo "> GIT FLOW"
+echo "---------------------------------------"
+
+if [[ $(dpkg -l | grep git-flow) = 'git-flow' ]];
+then 
+  apt-get install -y --only-upgrade git-flow 
+  echo "GIT FLOW already installed"
+else
+  apt-get install -y git-flow
+fi
